3c3ad87f1e3793e8ae31bedf2f53557c843ec51c
Adiittiiyaa <143860711+Adiittiiyaa@users.noreply.github.com>
2023-09-05 01:12:17 +0700

    Add files via upload

---
+++ balok1.py
@@ -0,0 +1,6 @@
+panjang = int(input('Masukkan panjang : '))
+lebar = int(input('Masukkan lebar : '))
+tinggi = int(input('Masukkan tinggi : '))
+
+volume = int(panjang*lebar*tinggi)
+print("Volume balok :", volume)
