9211999bfad39940f8badd43ba896db080dfd505
Adiittiiyaa <143860711+Adiittiiyaa@users.noreply.github.com>
2023-09-05 01:14:14 +0700

    Add files via upload

---
+++ bola1.py
@@ -0,0 +1,7 @@
+print("Menghitung luas dan volume bola")
+r=int(input("Masukkan Jari - Jari = "))
+phi = 22/7
+luas = 4/3*(phi*r*r*r)
+volume = 4*phi*r*r
+print("Luas Bola Adalah = ", luas)
+print("Volume Bola Adalah = ", volume)
